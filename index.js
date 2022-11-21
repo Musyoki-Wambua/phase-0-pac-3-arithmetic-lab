573947b690ed6f03a795c83ade3c855e882029de
Musyoki_Wambua <wjosephmusyoki@gmail.com>
2022-11-21 13:13:49 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,40 @@
+function add(){
+
+}
+
+function subtract(){
+
+}
+function multiply (){
+
+}
+function divide (){
+
+}
+function add (a = 10, b = 3){
+    return(a+b);
+}
+
+function subtract (a = 600 , b= 35){
+    return (a-b);
+}
+function multiply (a = 7 , b = 1){
+    return (a*b);    
+}
+function divide (a= 4, b= 1){
+    return (a/b);
+
+}
+
+function increment(n = 13 ){
+    return (n +=1 );
+}
+function decrement (d = 757){
+    return (d -=1 );
+}
+function makeInt (n = "900") {
+    return parseInt (n, 10);
+}
+function preserveDecimal (n = '2') {
+   return  parseFloat(n);
+}
